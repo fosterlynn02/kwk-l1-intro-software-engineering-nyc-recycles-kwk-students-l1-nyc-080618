65cca82d6c4198ce3f2948ec250614d7c48359c5
fosterlynn02 <fosterlynnboulin@gmail.com>
2018-08-09 15:38:34 +0000

    automatically backed up by learn

--- lib/recycle.rb
+++ lib/recycle.rb
@@ -5,6 +5,7 @@
 # We haven't hard coded the hashes in the tests, 
 #they're dynamically brought in from the NYC Open Data API
 
+
 # first_recycle_bin =  {
 #   "address"=>"E 227 St/Bronx River Pkway",
 #   "site_type"=>"Subproperty",
@@ -16,19 +17,48 @@
 
 
 #1. Define a method `address`, that takes a hash as an argument.
+
+def address (x)
+  return x["address"]
+end
+  
 #This method should return the value of the address key in the hash.
 
 #2. Define a method called `borough` that takes a hash as an argument.
 # This method should return the value of the borough key in the hash.
 
+def borough (x)
+  return x["borough"]
+end 
+
 #3. Define a method called `all_keys` that takes a hash as an argument.
 #This method should print out each key in the hash. 
 #You should use the `each_key` method to iterate over your hash and print out each key.
+
+def all_keys (x)
+  x.each_key do |key|
+    puts key
+  end
+end 
 
 #4. Define a method called `all_data` that takes a hash as an argument.
 #This method should print out each value in the hash.
 #You should use the `each_value` method to iterate over your hash and print out each value.
 
+def all_data(x)
+  x.each_value do |value|
+    puts value 
+  end 
+end
+  
+
 #5. Define a method called `key_and_data` that takes a hash as an argument.
 #This method should print out each key and value in the hash in a sentence like this: `The #{key} is #{value}`
 #You should use the `each` method to iterate over your hash to print out that sentence
+
+def key_and_data (x)
+  x.each do |key, value|
+    puts "The #{key} is #{value}"
+  end 
+end 
+  
